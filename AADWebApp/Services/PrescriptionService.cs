5243372db16e461307aa402ee7fe0636d90ae9f2
JackPawsey <43959681+JackPawsey@users.noreply.github.com>
2020-12-28 16:42:45 +0000

    Create PrescriptionService.cs

---
+++ AADWebApp/Services/PrescriptionService.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+
+namespace TestWebApp.Services
+{
+    public class PrescriptionService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public PrescriptionService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IEnumerable<int> GetPrescriptions()
+        {
+            IEnumerable<int> Prescriptions = null;
+
+            return Prescriptions;
+        }
+
+        public String CreatePrescription(int PatientID, String MedicationType, int Dosage, DateTime PrescriptionEnd)
+        {
+            String Result = "";
+
+            return Result;
+        }
+
+        public String CancelPrescription(int PrescriptionID)
+        {
+            String Result = "";
+
+            return Result;
+        }
+
+        public String SetPrescriptionMedication(int PrescriptionID, int Doasge)
+        {
+            String Result = "";
+
+            return Result;
+        }
+
+        public String SetPrescriptionCollectionDateTime(int PrescriptionID, DateTime DateTime)
+        {
+            String Result = "";
+
+            return Result;
+        }
+
+        public String SetPrescriptionStatus(int PrescriptionID, String Status)
+        {
+            String Result = "";
+
+            return Result;
+        }
+    }
+}
