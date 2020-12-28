400a8e8dc05195143880a69d656ba7b29811905e
JackPawsey <43959681+JackPawsey@users.noreply.github.com>
2020-12-28 16:43:58 +0000

    Create BloodTestResult.cs

---
+++ Models/BloodTestResult.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TestWebApp.Models
+{
+    public class BloodTestResult
+    {
+        public DateTime Occurance { get; set; }
+
+        public String BloodTestType { get; set; }
+
+        public int Result { get; set; }
+    }
+}
