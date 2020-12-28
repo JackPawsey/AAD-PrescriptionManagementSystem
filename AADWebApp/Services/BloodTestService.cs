699d130bc6ffa22b218eae9ed072bf60f4e1f7fc
JackPawsey <43959681+JackPawsey@users.noreply.github.com>
2020-12-28 16:41:53 +0000

    Create BloodTestService.cs

---
+++ AADWebApp/Services/BloodTestService.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace TestWebApp.Services
+{
+    public class BloodTestService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public BloodTestService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public String RequestBloodTest(int PatientID, String BloodTestType)
+        {
+            String Result = "";
+
+            return Result;
+        }
+
+        public String SetBloodTestDateTime(int BloodTestID, DateTime DateTime)
+        {
+            String Result = "";
+
+            return Result;
+        }
+
+        public String SetBloodTestResults(int BloodTestID, int[] TestResults)
+        {
+            String Result = "";
+
+            return Result;
+        }
+    }
+}
