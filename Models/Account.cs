fd47ff078e81c74e3277486f34e3c2b7c24a6ae7
JackPawsey <43959681+JackPawsey@users.noreply.github.com>
2020-12-28 16:44:34 +0000

    Update Account.cs

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace TestWebApp.Models
 {
-    public class BloodTest
+    public class Account
     {
-        public int BloodTestId { get; set; }
+        public int AccountId { get; set; }
 
-        public int PatientId { get; set; }
+        public string EmailAddress { get; set; }
 
-        public String BloodTestType { get; set; }
-
-        public DateTime DateRequested { get; set; }
+        public string Password { get; set; }
     }
 }
