f9e356b95fb1de386f929b06e4309d969aa9559d
JackPawsey <43959681+JackPawsey@users.noreply.github.com>
2020-12-28 16:39:11 +0000

    Create AccountService.cs

---
+++ AADWebApp/Services/AccountService.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+
+namespace TestWebApp.Services
+{
+    public class AccountService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public AccountService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public String Login(String Email, String Password)
+        {
+            String Result = "";
+
+            return Result;
+        }
+
+        public String Logout(int AccountID)
+        {
+            String Result = "";
+
+            return Result;
+        }
+
+        public IEnumerable<int> GetUsers()
+        {
+            IEnumerable<int> Users = null;
+
+            return Users;
+        }
+
+        public String AddUser(String Email, String Password, String AccountType)
+        {
+            String Result = "";
+
+            return Result;
+        }
+
+        public String RemoveUser(int AccountID)
+        {
+            String Result = "";
+
+            return Result;
+        }
+
+        public String SetPassword(int AccountID, String Password)
+        {
+            String Result = "";
+
+            return Result;
+        }
+    }
+}
