f7a395c95ee11db5ecfad89be45318b722317e33
Jack Pawsey <N0753949@my.ntu.ac.uk>
2020-12-29 16:07:58 +0000

    Add service to send emails

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using AADWebApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace AADWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<SendEmailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
