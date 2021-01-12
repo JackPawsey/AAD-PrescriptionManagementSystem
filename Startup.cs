3956de51c00c43b1ee21ad2cab0fb82881a9a440
Adam Watson <adamwatsonoa@gmail.com>
2021-01-12 16:54:44 +0000

    [S] Enable runtime Razor compilation

--- Startup.cs
+++ Startup.cs
@@ -19,7 +19,8 @@ namespace AADWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages()
+                    .AddRazorRuntimeCompilation();
             services.AddTransient<ISendEmailService, SendEmailService>();
             services.AddTransient<ISendSmsService, SendSmsService>();
         }
