6acdefee069f397003dc60cb5570e2782a94c008
Ryan Campbell <ryanjaycampbell@live.co.uk>
2021-01-25 17:40:31 +0000

    DatabaseService code cleanup.

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MySql.Data;
-using MySql.Data.MySqlClient;
-using System.Data.Sql;
 using System.Data.SqlClient;
 
 namespace AADWebApp.Services
