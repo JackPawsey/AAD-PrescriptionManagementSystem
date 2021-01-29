4942c86e042ce67d1b590935348da21e27f66512
Ryan Campbell <ryanjaycampbell@live.co.uk>
2021-01-29 14:36:31 +0000

    Added DatabaseService tests.

--- AADWebAppTests/Services/DatabaseServiceTests.cs
+++ AADWebAppTests/Services/DatabaseServiceTests.cs
@@ -3,40 +3,77 @@ using AADWebApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace AADWebApp.Services.Tests
 {
     [TestClass()]
     public class DatabaseServiceTests
     {
+        string Server = "cloud-crusaders-project-database-mssql.c8ratiay2jmd.eu-west-2.rds.amazonaws.com";
+        string Username = "admin";
+        //To-Do: Figure out secrets, change password.
+        string Password = "uPjz58%4";
+        string DatabaseName = "program_data";
+
+        DatabaseService TestService;
+
+        private DatabaseService CreateService()
+        {
+            return new DatabaseService(Server, Username, Password, DatabaseName);
+        }
+
         [TestMethod()]
         public void DatabaseServiceTest()
         {
-            throw new NotImplementedException();
+            CreateService();
         }
 
         [TestMethod()]
         public void ConnectToMSSQLServerTest()
         {
-            throw new NotImplementedException();
+            TestService = CreateService();
+            TestService.ConnectToMSSQLServer();
         }
 
         [TestMethod()]
         public void ExecuteQueryTest()
         {
-            throw new NotImplementedException();
+            ConnectToMSSQLServerTest();
+            string Query = "SELECT TOP(1) * FROM TestData";
+            SqlDataReader DataReader = TestService.ExecuteQuery(Query);
+            PrintDataReaderToConsole(DataReader);
         }
 
         [TestMethod()]
         public void ExecuteNonQueryTest()
         {
-            throw new NotImplementedException();
+            ConnectToMSSQLServerTest();
+            string NonQuery = "UPDATE TestData SET Value = 'ValueUpdated' WHERE Name = 'UpdateMe'";
+            Debug.WriteLine(TestService.ExecuteNonQuery(NonQuery));
         }
 
         [TestMethod()]
         public void RetrieveTableTest()
         {
-            throw new NotImplementedException();
+            ConnectToMSSQLServerTest();
+            SqlDataReader DataReader = TestService.RetrieveTable("TestData");
+            PrintDataReaderToConsole(DataReader);
+        }
+
+        private void PrintDataReaderToConsole(SqlDataReader Reader)
+        {
+            while (Reader.Read())
+            {
+                string output = "";
+                for (int i = 0; i < Reader.VisibleFieldCount; i++)
+                {
+                    output += Reader.GetString(i) + ", ";
+                }
+                Debug.WriteLine(output);
+            }
         }
     }
 }
