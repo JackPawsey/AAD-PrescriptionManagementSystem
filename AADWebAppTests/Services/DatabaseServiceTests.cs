d0dc1e605f8cf0e212978041bb173b45c15258d6
Ryan Campbell <ryanjaycampbell@live.co.uk>
2021-01-29 14:18:15 +0000

    Added unit testing project.

---
+++ AADWebAppTests/Services/DatabaseServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AADWebApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AADWebApp.Services.Tests
+{
+    [TestClass()]
+    public class DatabaseServiceTests
+    {
+        [TestMethod()]
+        public void DatabaseServiceTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void ConnectToMSSQLServerTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void ExecuteQueryTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void ExecuteNonQueryTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void RetrieveTableTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
