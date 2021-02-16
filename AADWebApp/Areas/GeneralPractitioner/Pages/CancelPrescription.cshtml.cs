c007873659342bbf2a08f05392f6964f8c789264
Jack Pawsey <N0753949@my.ntu.ac.uk>
2021-02-16 18:42:55 +0000

    Enable RBAC

--- AADWebApp/Areas/GeneralPractitioner/Pages/CancelPrescription.cshtml.cs
+++ AADWebApp/Areas/GeneralPractitioner/Pages/CancelPrescription.cshtml.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AADWebApp.Areas.GeneralPractitioner.Pages
 {
+    [Authorize(Roles = "General Practitioner, Admin")]
     public class CancelPrescriptionModel : PageModel
     {
         public void OnGet()
--- AADWebApp/Areas/Pharmacist/Pages/RequestBloodwork.cshtml.cs
+++ AADWebApp/Areas/Pharmacist/Pages/RequestBloodwork.cshtml.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AADWebApp.Areas.Pharmacist.Pages
 {
+    [Authorize(Roles = "Pharmacist, Admin")]
     public class RequestBloodworkModel : PageModel
     {
         public void OnGet()
