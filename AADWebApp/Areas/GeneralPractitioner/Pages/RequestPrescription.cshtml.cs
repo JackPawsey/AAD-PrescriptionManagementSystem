6d0b7304936aafcb5329caa8ff13664609fa7e74
Jack Pawsey <N0753949@my.ntu.ac.uk>
2021-02-16 16:17:23 +0000

    Add GP area

---
+++ AADWebApp/Areas/GeneralPractitioner/Pages/RequestPrescription.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AADWebApp.Areas.GeneralPractitioner.Pages
+{
+    public class RequestPrescriptionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
