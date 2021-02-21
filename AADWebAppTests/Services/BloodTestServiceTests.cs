36e8da20eeb39bd3dda09dd64859d268afd93c6b
Adam Watson <adamwatsonoa@gmail.com>
2021-02-21 01:50:21 +0000

    [S] Add unit tests for BloodTest and Medication services

---
+++ AADWebAppTests/Services/BloodTestServiceTests.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AADWebApp.Interfaces;
+using AADWebApp.Models;
+using AADWebApp.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using static AADWebApp.Services.DatabaseService;
+
+namespace AADWebAppTests.Services
+{
+    [TestClass]
+    public class BloodTestServiceTests : TestBase
+    {
+        private readonly IDatabaseService _databaseService;
+        private readonly IBloodTestService _bloodTestService;
+
+        public BloodTestServiceTests()
+        {
+            _databaseService = Get<IDatabaseService>();
+            _bloodTestService = new BloodTestService(_databaseService);
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            var databaseService = Get<IDatabaseService>();
+
+            // Pre-populate a prescription for later tests, which means also creating a user due to the table constraints
+            databaseService.ConnectToMssqlServer(AvailableDatabases.program_data);
+            databaseService.ExecuteNonQuery($"INSERT INTO patients (id, comm_preferences, nhs_number, general_practitioner) VALUES (1, 1, 1, 'gp-name');");
+            databaseService.ExecuteNonQuery($"INSERT INTO prescriptions (medication_id, patient_id, dosage, date_start, date_end, prescription_status, issue_frequency) VALUES (1, 1, 1, '{DateTime.Now:yyyy-MM-dd HH:mm:ss}', '{DateTime.Now:yyyy-MM-dd HH:mm:ss}', 'Approved', 'Weekly');");
+            databaseService.ExecuteNonQuery($"INSERT INTO prescriptions (medication_id, patient_id, dosage, date_start, date_end, prescription_status, issue_frequency) VALUES (1, 1, 1, '{DateTime.Now:yyyy-MM-dd HH:mm:ss}', '{DateTime.Now:yyyy-MM-dd HH:mm:ss}', 'Approved', 'Weekly');");
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            var databaseService = Get<IDatabaseService>();
+
+            // Clear the tables
+            databaseService.ConnectToMssqlServer(AvailableDatabases.program_data);
+            databaseService.ExecuteNonQuery($"DELETE FROM prescriptions;");
+            databaseService.ExecuteNonQuery($"DELETE FROM patients;");
+
+            // Reset auto increment values ready for next tests
+            databaseService.ExecuteNonQuery($"DBCC CHECKIDENT (prescriptions, RESEED, 0);");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _databaseService.ConnectToMssqlServer(AvailableDatabases.program_data);
+            _databaseService.ExecuteNonQuery($"DELETE FROM blood_test_results");
+            _databaseService.ExecuteNonQuery($"DELETE FROM blood_test_requests");
+            _databaseService.ExecuteNonQuery($"DBCC CHECKIDENT (blood_test_results, RESEED, 0);");
+            _databaseService.ExecuteNonQuery($"DBCC CHECKIDENT (blood_test_requests, RESEED, 0);");
+        }
+
+        [TestMethod]
+        public void WhenThereAreBloodTests()
+        {
+            var results = _bloodTestService.GetBloodTests();
+
+            Assert.IsTrue(results.Any());
+        }
+
+        [TestMethod]
+        public void WhenGettingBloodTestByAValidId()
+        {
+            // Prep Expected
+            IEnumerable<BloodTest> expected = new List<BloodTest>
+            {
+                new BloodTest
+                {
+                    Id = 6,
+                    AbbreviatedTitle = "FBC",
+                    FullTitle = "Full Blood Count",
+                    RestrictionLevel = 1
+                }
+            };
+
+            var expectedSerialised = JsonConvert.SerializeObject(expected);
+
+            // GetBloodTests with id 6
+            var results = _bloodTestService.GetBloodTests(6);
+
+            // Check results
+            var resultSerialised = JsonConvert.SerializeObject(results);
+
+            Assert.IsTrue(results.Count() == 1);
+            Assert.AreEqual(expectedSerialised, resultSerialised);
+        }
+
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(1000)]
+        public void WhenGettingBloodTestByAnInvalidId(int id)
+        {
+            var results = _bloodTestService.GetBloodTests((short) id);
+
+            Assert.IsTrue(!results.Any());
+        }
+
+        // GetBloodTestResults test methods
+        [TestMethod]
+        public void WhenThereAreNoBloodTestResults()
+        {
+            var results = _bloodTestService.GetBloodTestResults();
+
+            Assert.IsTrue(!results.Any());
+        }
+
+        // GetBloodTestRequests test methods
+        [TestMethod]
+        public void WhenThereAreNoBloodTestRequests()
+        {
+            var results = _bloodTestService.GetBloodTestRequests();
+
+            Assert.IsTrue(!results.Any());
+        }
+
+        // RequestBloodTest test method
+        [TestMethod]
+        public void WhenRequestingBloodTestsRowsAreAdded()
+        {
+            var beforeRequestResults = _bloodTestService.GetBloodTestRequests().ToList();
+            Assert.IsTrue(!beforeRequestResults.Any());
+
+            var now = DateTime.Now;
+
+            // Prep expected
+            IEnumerable<BloodTestRequest> allExpected = new List<BloodTestRequest>
+            {
+                new BloodTestRequest
+                {
+                    Id = 1,
+                    PrescriptionId = 1,
+                    BloodTestId = 1,
+                    AppointmentTime = now
+                },
+                new BloodTestRequest
+                {
+                    Id = 2,
+                    PrescriptionId = 2,
+                    BloodTestId = 3,
+                    AppointmentTime = now
+                },
+                new BloodTestRequest
+                {
+                    Id = 3,
+                    PrescriptionId = 1,
+                    BloodTestId = 1,
+                    AppointmentTime = now
+                }
+            };
+
+            IEnumerable<BloodTestRequest> expected1 = new List<BloodTestRequest>
+            {
+                new BloodTestRequest
+                {
+                    Id = 1,
+                    PrescriptionId = 1,
+                    BloodTestId = 1,
+                    AppointmentTime = now
+                },
+                new BloodTestRequest
+                {
+                    Id = 3,
+                    PrescriptionId = 1,
+                    BloodTestId = 1,
+                    AppointmentTime = now
+                }
+            };
+
+            IEnumerable<BloodTestRequest> expected2 = new List<BloodTestRequest>
+            {
+                new BloodTestRequest
+                {
+                    Id = 2,
+                    PrescriptionId = 2,
+                    BloodTestId = 3,
+                    AppointmentTime = now
+                }
+            };
+
+            var allExpectedSerialised = JsonConvert.SerializeObject(allExpected, new JsonSerializerSettings
+            {
+                DateFormatString = "yyyy-MM-ddTHH:mm:ss"
+            });
+
+            var expected1Serialised = JsonConvert.SerializeObject(expected1, new JsonSerializerSettings
+            {
+                DateFormatString = "yyyy-MM-ddTHH:mm:ss"
+            });
+
+            var expected2Serialised = JsonConvert.SerializeObject(expected2, new JsonSerializerSettings
+            {
+                DateFormatString = "yyyy-MM-ddTHH:mm:ss"
+            });
+
+            // Request blood tests
+            var affectedRows1 = _bloodTestService.RequestBloodTest(1, 1, now);
+            Assert.AreEqual(1, affectedRows1);
+
+            var affectedRows2 = _bloodTestService.RequestBloodTest(2, 3, now);
+            Assert.AreEqual(1, affectedRows2);
+
+            var affectedRows3 = _bloodTestService.RequestBloodTest(1, 1, now);
+            Assert.AreEqual(1, affectedRows3);
+
+            // Check results - GetBloodTestRequests with no id
+            var afterRequestResults = _bloodTestService.GetBloodTestRequests();
+            var afterRequestResultSerialised = JsonConvert.SerializeObject(afterRequestResults);
+
+            Assert.IsTrue(afterRequestResults.Count() == 3);
+            Assert.AreEqual(allExpectedSerialised, afterRequestResultSerialised);
+
+            // Check results - GetBloodTestRequests with id expecting 2 results
+            var afterRequestResultsById1 = _bloodTestService.GetBloodTestRequests(1);
+            var afterRequestResultsById1Serialised = JsonConvert.SerializeObject(afterRequestResultsById1);
+
+            Assert.IsTrue(afterRequestResultsById1.Count() == 2);
+            Assert.AreEqual(expected1Serialised, afterRequestResultsById1Serialised);
+
+            // Check results - GetBloodTestRequests with id expecting 1 result
+            var afterRequestResultsById2 = _bloodTestService.GetBloodTestRequests(2);
+            var afterRequestResultsById2Serialised = JsonConvert.SerializeObject(afterRequestResultsById2);
+
+            Assert.IsTrue(afterRequestResultsById2.Count() == 1);
+            Assert.AreEqual(expected2Serialised, afterRequestResultsById2Serialised);
+        }
+
+        // SetBloodTestDateTime test method
+        [TestMethod]
+        public void WhenSettingsAnAppointmentTimeTheRowIsUpdated()
+        {
+            // Prerequisite setup
+            var initialTime = DateTime.Now;
+            var updatedTime = initialTime.AddDays(1);
+
+            _bloodTestService.RequestBloodTest(1, 1, initialTime);
+
+            // Begin actual test
+            // Prep expected
+            IEnumerable<BloodTestRequest> expectBeforeUpdate = new List<BloodTestRequest>
+            {
+                new BloodTestRequest
+                {
+                    Id = 1,
+                    PrescriptionId = 1,
+                    BloodTestId = 1,
+                    AppointmentTime = initialTime
+                }
+            };
+
+            var beforeExpectedSerialised = JsonConvert.SerializeObject(expectBeforeUpdate, new JsonSerializerSettings
+            {
+                DateFormatString = "yyyy-MM-ddTHH:mm:ss"
+            });
+
+            IEnumerable<BloodTestRequest> expectedAfterUpdate = new List<BloodTestRequest>
+            {
+                new BloodTestRequest
+                {
+                    Id = 1,
+                    PrescriptionId = 1,
+                    BloodTestId = 1,
+                    AppointmentTime = updatedTime
+                }
+            };
+
+            var afterExpectedSerialised = JsonConvert.SerializeObject(expectedAfterUpdate, new JsonSerializerSettings
+            {
+                DateFormatString = "yyyy-MM-ddTHH:mm:ss"
+            });
+
+            // Check prior to adding
+            var beforeUpdateResults = _bloodTestService.GetBloodTestRequests();
+            var beforeUpdateResultSerialised = JsonConvert.SerializeObject(beforeUpdateResults);
+
+
+            Assert.IsTrue(beforeUpdateResults.Count() == 1);
+            Assert.AreEqual(beforeExpectedSerialised, beforeUpdateResultSerialised);
+            Assert.AreEqual(initialTime, expectBeforeUpdate.First().AppointmentTime);
+
+            // Update time
+            var affectedRows = _bloodTestService.SetBloodTestDateTime(1, updatedTime);
+            Assert.AreEqual(1, affectedRows);
+
+            // // Check results - GetBloodTestRequests with no id
+            var afterUpdateResults = _bloodTestService.GetBloodTestRequests();
+            var afterUpdateResultSerialised = JsonConvert.SerializeObject(afterUpdateResults);
+
+            Assert.IsTrue(afterUpdateResults.Count() == 1);
+            Assert.AreEqual(afterExpectedSerialised, afterUpdateResultSerialised);
+            Assert.AreEqual(updatedTime, expectedAfterUpdate.First().AppointmentTime);
+
+            // Check results - GetBloodTestRequests with id
+            var afterUpdateResultsById = _bloodTestService.GetBloodTestRequests(1);
+            var afterUpdateResultByIdSerialised = JsonConvert.SerializeObject(afterUpdateResultsById);
+
+            Assert.IsTrue(afterUpdateResultsById.Count() == 1);
+            Assert.AreEqual(afterExpectedSerialised, afterUpdateResultByIdSerialised);
+            Assert.AreEqual(updatedTime, expectedAfterUpdate.First().AppointmentTime);
+        }
+
+        // SetBloodTestResults test method
+        [TestMethod]
+        public void WhenAddingBloodTestResultsRowsAreAdded()
+        {
+            // Prerequisite setup
+            var now = DateTime.Now;
+
+            _bloodTestService.RequestBloodTest(1, 1, now);
+            _bloodTestService.RequestBloodTest(1, 2, now);
+
+            // Begin actual test
+            var beforeResultResults = _bloodTestService.GetBloodTestResults().ToList();
+            Assert.IsTrue(!beforeResultResults.Any());
+
+            // Prep expected
+            IEnumerable<BloodTestResult> allExpected = new List<BloodTestResult>
+            {
+                new BloodTestResult
+                {
+                    Id = 1,
+                    BloodTestId = 1,
+                    Result = false,
+                    ResultTime = now
+                },
+                new BloodTestResult
+                {
+                    Id = 2,
+                    BloodTestId = 2,
+                    Result = true,
+                    ResultTime = now
+                }
+            };
+
+            IEnumerable<BloodTestResult> expected1 = new List<BloodTestResult>
+            {
+                new BloodTestResult
+                {
+                    Id = 2,
+                    BloodTestId = 2,
+                    Result = true,
+                    ResultTime = now
+                }
+            };
+
+            var allExpectedSerialised = JsonConvert.SerializeObject(allExpected, new JsonSerializerSettings
+            {
+                DateFormatString = "yyyy-MM-ddTHH:mm:ss"
+            });
+
+            var expected1Serialised = JsonConvert.SerializeObject(expected1, new JsonSerializerSettings
+            {
+                DateFormatString = "yyyy-MM-ddTHH:mm:ss"
+            });
+
+            // Set blood test results
+            var affectedRows1 = _bloodTestService.SetBloodTestResults(1, false, now);
+            Assert.AreEqual(1, affectedRows1);
+
+            var affectedRows2 = _bloodTestService.SetBloodTestResults(2, true, now);
+            Assert.AreEqual(1, affectedRows2);
+
+            // Check results - GetBloodTestResults with no id
+            var afterResultResults = _bloodTestService.GetBloodTestResults();
+            var afterResultResultsSerialised = JsonConvert.SerializeObject(afterResultResults);
+
+            Assert.IsTrue(afterResultResults.Count() == 2);
+            Assert.AreEqual(allExpectedSerialised, afterResultResultsSerialised);
+
+            // Check results - GetBloodTestResults with id
+            var afterResultResultsById = _bloodTestService.GetBloodTestResults(2);
+            var afterResultResultsByIdSerialised = JsonConvert.SerializeObject(afterResultResultsById);
+
+            Assert.IsTrue(afterResultResultsById.Count() == 1);
+            Assert.AreEqual(expected1Serialised, afterResultResultsByIdSerialised);
+        }
+    }
+}
---
+++ AADWebAppTests/Services/MedicationServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AADWebApp.Interfaces;
+using AADWebApp.Models;
+using AADWebApp.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace AADWebAppTests.Services
+{
+    [TestClass]
+    public class MedicationServiceTests : TestBase
+    {
+        private readonly IMedicationService _medicationService;
+
+        public MedicationServiceTests()
+        {
+            _medicationService = new MedicationService(Get<IDatabaseService>());
+        }
+
+        [TestMethod]
+        public void WhenThereAreMedications()
+        {
+            var results = _medicationService.GetMedications();
+
+            Assert.IsTrue(results.Any());
+        }
+
+        [TestMethod]
+        public void WhenGettingMedicationById()
+        {
+            // Prep expected
+            IEnumerable<Medication> expected = new List<Medication>
+            {
+                new Medication
+                {
+                    Id = 2,
+                    MedicationName = "Olanzapine",
+                    BloodWorkRestrictionLevel = 2
+                }
+            };
+
+            var expectedSerialised = JsonConvert.SerializeObject(expected);
+
+            // GetMedications with id 2
+            var results = _medicationService.GetMedications(2);
+
+            // Check results
+            var resultSerialised = JsonConvert.SerializeObject(results);
+
+            Assert.IsTrue(results.Count() == 1);
+            Assert.AreEqual(expectedSerialised, resultSerialised);
+        }
+    }
+}
