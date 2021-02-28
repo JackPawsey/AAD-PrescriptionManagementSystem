3cd2823dbcd649acbeb8767f20685cd8a6899363
Adam Watson <adamwatsonoa@gmail.com>
2021-02-28 22:51:57 +0000

    [S] Admin can issue password reset for users

--- AADWebApp/Areas/Admin/Pages/ListUsers.cshtml.cs
+++ AADWebApp/Areas/Admin/Pages/ListUsers.cshtml.cs
@@ -1,13 +1,17 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using AADWebApp.Areas.Identity.Data;
+using AADWebApp.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace AADWebApp.Areas.Admin.Pages
 {
@@ -16,6 +20,7 @@ namespace AADWebApp.Areas.Admin.Pages
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ISendEmailService _sendEmailService;
 
         [BindProperty]
         public InputModel Input { get; set; }
@@ -23,10 +28,11 @@ namespace AADWebApp.Areas.Admin.Pages
         [BindProperty]
         public string UserId { get; set; }
 
-        public ListUsersModel(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        public ListUsersModel(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ISendEmailService sendEmailService)
         {
             _userManager = userManager;
             _roleManager = roleManager;
+            _sendEmailService = sendEmailService;
 
             Roles = new SelectList(_roleManager.Roles.ToList());
         }
@@ -45,7 +51,7 @@ namespace AADWebApp.Areas.Admin.Pages
         {
         }
 
-        public async Task<IActionResult> OnPost()
+        public async Task<IActionResult> OnPostDeleteUserAsync()
         {
             Console.WriteLine("UserID to delete: " + UserId);
 
@@ -60,6 +66,42 @@ namespace AADWebApp.Areas.Admin.Pages
                 else
                     foreach (var error in deleteResult.Errors)
                         ModelState.AddModelError("", error.Description);
+            }
+            else
+            {
+                ModelState.AddModelError("", "User not found");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostIssuePasswordResetAsync()
+        {
+            Console.WriteLine("UserID to issue password reset for: " + UserId);
+
+            var user = await _userManager.FindByIdAsync(UserId);
+
+            if (user != null)
+            {
+                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Page(
+                    "/Account/ResetPassword",
+                    null,
+                    new
+                    {
+                        area = "Identity",
+                        code
+                    },
+                    Request.Scheme);
+
+                _sendEmailService.SendEmail(
+                    $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.",
+                    "Reset Password",
+                    user.Email
+                );
+
+                TempData["PasswordResetSuccess"] = $"Password reset issued for {user.Email}.";
             }
             else
             {
